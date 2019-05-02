d3ae6e2f74f9276c8709afc2d07df3a755bcea09
Reina Ewing <reina.castro3@gmail.com>
2019-05-02 15:22:24 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,6 +5,10 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def position_taken?(board, index)
     if board[index] == " " || board[index] == "" || board[index] == nil
      return false
+<<<<<<< HEAD
+=======
+     
+>>>>>>> 25a4ac260b826404a060c44c9973f697adea058c
    else
      return true 
     end
